b204b2d0d7a08066e7a7c0e3e7c98c2906ce6ec4
MatHeiUit <98590132+MatHeiUit@users.noreply.github.com>
2023-09-11 14:09:06 +0200

    Fixed directory issue

--- Task 3/baseconfig.py
+++ Task 3/baseconfig.py
@@ -3,5 +3,4 @@ import sys, os
 
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = "/uploads"
-app.config['DOWNLOAD_FOLDER'] = "/download"
+app.config['UPLOAD_FOLDER'] = os.path.join(sys.path[0]) + "/uploads"
