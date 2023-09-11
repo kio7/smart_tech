b78c2a6c08b03156979dabc4a7ebcd2035eeac54
MatHeiUit <98590132+MatHeiUit@users.noreply.github.com>
2023-09-11 11:10:51 +0200

    Interactive with input
    Can choose any of the possible options from the CLI.

--- Task 3/Pyramids.py
+++ Task 3/Pyramids.py
@@ -32,27 +32,35 @@ def Laplacian(filename, num):
 
 def main():
     """
-    To use this program, comment out the method and accomadating variables you do not wish to use. 
-    Currently the Laplacian method and variables are commented out, so it's set up to use the Guassian pyramid.
+    To use this program, provide the necessary input.
     """
-
+    
     # "cat" can be changed out for "space" or "house", these are other pictures i have included. However, only use Gaussian for "space".
     # The reason why you do not want to use the Laplacian pyramid on the picture "space" is the color simularities are too high. The result doesn't make any sense to use.
-    filename = os.path.join(os.path.dirname(__file__), "cat.jpg")
+    image_choice = int(input("Which image do you want to see?\n1) 'Cat'\n2) 'Space'\n3) 'House'\n\nEnter 1, 2, or 3: \n"))
+    match image_choice:
+        case 1:
+            filename = os.path.join(os.path.dirname(__file__), "cat.jpg")
+        case 2:
+            filename = os.path.join(os.path.dirname(__file__), "space.jpg")
+        case 3:
+            filename = os.path.join(os.path.dirname(__file__), "house.jpg")
 
-    """ Gaussian """
-    amount = 8
-    cols = 3
-    rows = 3
-    lis = Gaussian(filename, amount)
-
-
-    """ Laplacian """
-    # I'm only doing 3 compressions, because after that point Laplacian doesn't make sense. As it all turns into the same color.
-    # amount = 3
-    # cols = 2
-    # rows = 2
-    # lis = Laplacian(filename, amount)
+    method_choice = int(input("Which method do you wish to use?\n1) Gaussian\n2) Laplacian\n\nEnter 1 or 2: \n"))
+    match method_choice:
+        case 1:
+            """ Gaussian """
+            amount = 8
+            cols = 3
+            rows = 3
+            lis = Gaussian(filename, amount)
+        case 2:
+            """ Laplacian """
+            # I'm only doing 3 compressions, because after that point Laplacian doesn't make sense. As it all turns into the same color.
+            amount = 3
+            cols = 2
+            rows = 2
+            lis = Laplacian(filename, amount)
 
 
     fig = plt.figure(figsize = (16, 9)) # Screen ratio
