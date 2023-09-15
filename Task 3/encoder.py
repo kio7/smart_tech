b8d976f6b5211c7aa8d6b7f6f970706119f544ef
MatHeiUit <98590132+MatHeiUit@users.noreply.github.com>
2023-09-15 14:43:43 +0200

    Added RLE

--- Task 3/encoder.py
+++ Task 3/encoder.py
@@ -1,12 +1,30 @@
 # TODO Write a class that holds methods for the 4 different algorithms.
 
 class Encoder():
-    def __init__(self, file, type): 
+    def __init__(self, file, type, data): 
         self.file = file # Pass a file as an argument? Alternatively raw data?
         self.type = type # Filetype?
+        self.data = data
 
     def RLE(self):
-        pass
+        data = self.data
+        encoded_string = ""
+        i = 0
+        while (i <= len(data)-1):
+            count = 1
+            ch = data[i]
+            j = i
+            '''if the character at the current index is the same as the character at the next index. If the characters are the same, the count is incremented to 1'''
+            while (j < len(data)-1):
+                if (data[j] == data[j + 1]): 
+                    count = count + 1
+                    j = j + 1
+                else: 
+                    break
+            '''the count and the character is concatenated to the encoded string'''
+            encoded_string = encoded_string + str(count) + ch
+            i = j + 1
+        return encoded_string
     
 
     def Arithmetic(self):
