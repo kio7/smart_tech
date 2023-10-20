22831976fa38ed03630a4c161515057c9bedf220
MatHeiUit <98590132+MatHeiUit@users.noreply.github.com>
2023-10-20 14:38:08 +0200

    Routes & HTML
    For task 5 and 6

--- Submission 2/__init__.py
+++ Submission 2/__init__.py
@@ -7,5 +7,14 @@ import cv2
 def index():
     return render_template("index.html")
 
+@app.route("/harnverhalt", methods=["GET"])
+def harnverhalt():
+    return render_template("harnverhalt.html")
+
+@app.route("/cmp", methods=["GET"])
+def cmp():
+    return render_template("cmp.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
