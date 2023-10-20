1140e97a8db9044428d09071724cec9cddf3f3ff
Trombasso <anders@karlskaas.no>
2023-10-20 14:39:56 +0200

    satt opp routes

--- Submission 2/__init__.py
+++ Submission 2/__init__.py
@@ -7,5 +7,38 @@ import cv2
 def index():
     return render_template("index.html")
 
+@app.route("/lossy-compression", methods=["GET"])
+def lossy_compression():
+    # Task 1
+
+
+    return render_template("index.html")
+
+@app.route("/lossy-compression-2", methods=["GET"])
+def lossy_compression_2():
+    # Task 2
+    
+    return render_template("index.html")
+
+@app.route("/wavelet-mra", methods=["GET"])
+def wavelet_mra():
+    # Task 3
+    
+    return render_template("index.html")
+
+@app.route("/wavelet_transform", methods=["GET"])
+def wavelet_transform():
+    # Task 4
+    
+    return render_template("index.html")
+
+@app.route("/harnverhalt", methods=["GET"])
+def harnverhalt():
+    # Task 4
+    
+    return render_template("index.html")
+
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
